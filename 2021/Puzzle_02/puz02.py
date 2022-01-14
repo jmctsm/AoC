2fec7a1eb838192e95ea7de0692f9765e5b9ac6e
jmctsm <james.copeland@gmail.com>
2022-01-14 12:01:50 -0600

    Changes to make work. Still not.

--- 2021/Puzzle_02/puz02.py
+++ 2021/Puzzle_02/puz02.py
@@ -1,12 +1,64 @@
 #!python
 
+from typing import List
+
+
+def read_file() -> List[int]:
+    return_list = []
+    with open(f"input.txt", "r", encoding="utf-8") as input_file:
+        lines = input_file.readlines()
+        for line in lines:
+            split_list = line.split()
+            return_list.append([split_list[0], int(split_list[1])])
+    return return_list
+
 
 def puz_part01():
-    pass
+    input_list = read_file()
+    forward_location = 0
+    depth_location = 0
+    for movement_list in input_list:
+        if movement_list[0] == "forward":
+            forward_location += movement_list[1]
+        elif movement_list[0] == "up":
+            depth_location -= movement_list[1]
+        elif movement_list[0] == "down":
+            depth_location += movement_list[1]
+        else:
+            print(movement_list[0])
+            exit()
+    print("Part 01")
+    print(f"Forward Movement = {forward_location} ")
+    print(f"Depth = {depth_location} ")
+    print(f"forward x depth = {forward_location * depth_location}")
+    print("\n\n")
 
 
 def puz_part02():
-    pass
+    input_list = read_file()
+    forward_location = 0
+    depth_location = 0
+    aim_location = 0
+    for movement_list in input_list:
+        if movement_list[0] == "forward":
+            forward_location += movement_list[1]
+            if aim_location != 0:
+                depth_location = depth_location + (aim_location * forward_location)
+            elif aim_location == 0:
+                depth_location = depth_location
+        elif movement_list[0] == "up":
+            aim_location -= movement_list[1]
+        elif movement_list[0] == "down":
+            aim_location += movement_list[1]
+        else:
+            print(movement_list[0])
+            exit()
+    print("Part 02")
+    print(f"Forward Movement = {forward_location} ")
+    print(f"Depth = {depth_location} ")
+    print(f"Aim = {aim_location} ")
+    print(f"forward x depth = {forward_location * depth_location}")
+    print("\n\n")
 
 
 def main():
