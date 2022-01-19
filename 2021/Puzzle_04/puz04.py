097b8cf193277b80062c3ed24f979239617b398c
jmctsm <james.copeland@gmail.com>
2022-01-18 20:44:47 -0600

    Puzzle 4 start

---
+++ 2021/Puzzle_04/puz04.py
@@ -0,0 +1,21 @@
+#!python
+
+
+def puz_part01():
+
+    print("Part 01")
+    print("\n\n")
+
+
+def puz_part02():
+    print("Part 02")
+    print("\n\n")
+
+
+def main():
+    puz_part01()
+    puz_part02()
+
+
+if __name__ == "__main__":
+    main()
