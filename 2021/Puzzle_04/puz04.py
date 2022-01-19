16f1182c46076c4ea3302545aab5b9654c957c39
jmctsm <james.copeland@gmail.com>
2022-01-18 21:33:13 -0600

    Puz4 started and with input

--- 2021/Puzzle_04/puz04.py
+++ 2021/Puzzle_04/puz04.py
@@ -1,8 +1,49 @@
 #!python
 
 
-def puz_part01():
+def readfile_input():
+    boards_return = []
+    call_list_return = []
+    game_board = []
+    with open("input.txt", "r", encoding="utf-8") as input_file:
+        lines = input_file.readlines()
+        line_list = []
+        line_counter = 0
+        for line in lines:
+            stripped_line = line.strip()
+            if stripped_line == "":
+                if len(line_list) > 5:
+                    call_list_return = line_list
+                    line_list = []
+                    continue
+                elif len(line_list) == 5:
+                    game_board.append(line_list)
+                    if line_counter == 4:
+                        boards_return.append(game_board)
+                        line_counter = 0
+                        game_board = []
+                        line_list = []
+                    else:
+                        line_counter += 1
+                    continue
+                else:
+                    print("line_list is not the right number")
+                    print(f"{line_list = }")
+                    exit()
+            elif "," in stripped_line:
+                line_list = stripped_line.split(",")
+            else:
+                line_list = stripped_line.split()
+    return (call_list_return, boards_return)
 
+
+def puz_part01():
+    call_list, input_list = readfile_input()
+    print(call_list)
+    for game_board in input_list:
+        for game_line in game_board:
+            print(game_line)
+        print("\n\n")
     print("Part 01")
     print("\n\n")
 
